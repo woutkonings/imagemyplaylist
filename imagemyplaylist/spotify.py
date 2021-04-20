b4e7664fc8d4c98a8d74da6cdf523885f9461cc4
Wout Konings <woutkonings@gmail.com>
2021-04-21 00:19:09 +0200

    genres getter toegevoegd, maar de audio feature werkt nog niet voor meer dan 100 tracks

--- imagemyplaylist/spotify.py
+++ imagemyplaylist/spotify.py
@@ -10,6 +10,10 @@ import requests
 import config
 import base64
 import pandas as pd
+import numpy as np
+
+
+# https://github.com/vanortg/Flask-Spotify-Auth
 
 
 def get_token(client_id, client_secret):
@@ -66,7 +70,7 @@ def get_playlist(playlist_id, token):
 
     """
     
-    playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}"
+    playlistUrl = f"https://api.spotify.com/v1/playlists/{playlist_id}"
     headers = {
     "Authorization": "Bearer " + token
     }
@@ -76,11 +80,35 @@ def get_playlist(playlist_id, token):
     
     return playlist_dict
 
-def get_songs_feature(playlist_dict, feature):
+def get_playlist_tracks(playlist_id, token):
+    #TODO: method comment
     
+    playlistUrl = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    headers = {
+    "Authorization": "Bearer " + token
+    }
+    
+    res_list = []
+    
+    offset = 0
+    list_length = 1
+    while list_length != 0:
+        params = {'offset' : offset}
+        res = requests.get(url=playlistUrl, headers=headers, params=params)
+        r = res.json()['items']
+        list_length = len(r)
+        res_list = res_list + r
+        offset += 100
+    
+    return res_list
+
+
+def get_songs_feature(playlist_dict, feature):
+    #TODO: method comment
     return [x['track'][feature] for x in playlist_dict['tracks']['items']]
 
 def get_audio_features(song_ids, token):
+    #TODO: method comment
     
     headers = {
     "Authorization": "Bearer " + token
@@ -91,13 +119,13 @@ def get_audio_features(song_ids, token):
     text_song_ids = ",".join(song_ids)
     payload = {'ids' : text_song_ids}
     
-    #TODO: Waarom werkt deze tori niet....
+    #TODO: Ik heb een vermoeden dat deze ook niet meer dan 100 nummers als input kan nemen
     res = requests.get(url=base_url, headers=headers, params=payload)
     r = res.json()
-    return r
+    return r['audio_features']
 
 def get_audio_analysis(song_id, token):
-    
+    #TODO: method comment
     
     headers = {
     "Authorization": "Bearer " + token
@@ -111,6 +139,58 @@ def get_audio_analysis(song_id, token):
     output = { key: r[key] for key in output_features}
     return output
 
+
+def get_genres(artist_ids, token):
+    #TODO: method comment
+    headers = {
+    "Authorization": "Bearer " + token
+    }
+    
+    base_url = 'https://api.spotify.com/v1/artists'
+    num_of_artists = len(artist_ids)
+    
+    #Make artist requests per 50
+    res_list = []
+    d = int(np.floor(num_of_artists / 50))
+    for i in range(1, d + 2):
+        query_list = artist_ids[(i-1) * 50 : i * 50]
+        text_query_list = ",".join(query_list)
+        payload = {'ids' : text_query_list}
+        res = requests.get(url=base_url, headers=headers, params=payload)
+        res_list = res_list + res.json()['artists']
+    
+    res_list = [x['genres'] for x in res_list]
+    
+    return res_list
+    
+
+def get_song_df(playlist_id, token):
+    #TODO: method comment
+    playlist_dict = get_playlist(playlist_id, token)
+    playlist_dict['tracks']['items'] = get_playlist_tracks(playlist_id, token)
+    song_ids = [x['track']['id'] for x in playlist_dict['tracks']['items']]
+    song_titles = [x['track']['name'] for x in playlist_dict['tracks']['items']]
+    song_artists = [x['track']['artists'][0]['name'] for x in playlist_dict['tracks']['items']]
+    song_artists_ids = [x['track']['artists'][0]['id'] for x in playlist_dict['tracks']['items']]
+    genres = get_genres(song_artists_ids, token)
+    
+    df = pd.DataFrame()
+    df['ID'] = song_ids
+    df['Title'] = song_titles
+    df['Artist'] = song_artists
+    df['ArtistID'] = song_artists
+    df['Genres'] = genres
+    
+    # get the audio features and put in dataframe format
+    df_add = pd.DataFrame(index = df.index)
+    s = get_audio_features(song_ids, token)
+    df_add['feature'] = get_audio_features(song_ids, token)
+    df_add = pd.json_normalize(df_add['feature'])
+    
+    df = pd.concat([df, df_add], axis=1)
+    
+    return df
+
 if __name__ == "__main__":
     
     spotify_client_id = config.SPOTIFY_CLIENT_ID
@@ -118,21 +198,10 @@ if __name__ == "__main__":
     token = get_token(spotify_client_id, spotify_client_secret)
     config.set_global('TOKEN', token)
     
-    playlistId = "66yuBIG6Ko7topxRJ1b3iq"
-    playlist_dict = get_playlist(playlistId, config.TOKEN)
-    tracks = playlist_dict['tracks']['items'][0]['track']
-    song_ids = get_songs_feature(playlist_dict, 'id')
-    song_titles = get_songs_feature(playlist_dict, 'name')
-    song_artists = [x[0]['name'] for x in get_songs_feature(playlist_dict, 'artists')]
+    # playlistId = "4J7qSdpBBzCWO9n3kbQIJg" #Disco playlist with 148 songs
+    playlistId = "55oXRfL4wQCPxFMHY3ReFo" # Electro playlist with 31 songs
     
-    df = pd.DataFrame()
-    df['ID'] = song_ids
-    df['Title'] = song_titles
-    df['Artist'] = song_artists
+    df = get_song_df(playlistId, config.TOKEN)
     
-    df2 = pd.DataFrame()
-    df2['analysis'] = df['ID'].apply(lambda x : get_audio_analysis(x, config.TOKEN))
-    d = pd.json_normalize(df2['analysis'])
-    
-    df = pd.concat([df, d], axis=1)
+
     
