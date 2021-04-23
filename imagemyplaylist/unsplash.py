c5d81e513c1d8c8984dd13e159f8a516b282f1f6
Wout Konings <woutkonings@gmail.com>
2021-04-23 23:55:55 +0200

    finished unsplash query and download

--- imagemyplaylist/unsplash.py
+++ imagemyplaylist/unsplash.py
@@ -25,14 +25,31 @@ class Unsplash():
                         }
         
         
-    def query(self, query):
+    def query(self, query, page = 1, per_page = 9):
+        
+        params = {'page' : page,
+                  'per_page' : per_page}
         
         req_url = f'https://api.unsplash.com/search/photos?page=1&query={query}'
-        res = requests.get(url=req_url, headers=self.headers)
+        res = requests.get(url=req_url, headers=self.headers, params=params)
         r = res.json()
         
         return r
-
+    
+    def query_to_display_urls(self, query_result, size = 'small'):
+        
+        # for x in query_result['results']:
+        #     print(x['alt_description'])
+        #     print(x['urls'][size])
+            
+        return {x['alt_description'] : x['urls'][size] for x in query_result['results']}
+    
+    def save_pictures(self, url_dict, location):
+        
+        for name, url in url_dict.items():
+            r = requests.get(url, allow_redirects=True)
+            open(f'{name}.jpeg', 'wb').write(r.content)
+    
 if __name__ == "__main__":
     
     
@@ -44,3 +61,5 @@ if __name__ == "__main__":
     
     us = Unsplash()
     r = us.query(query)
+    q = us.query_to_display_urls(r)
+    # us.save_pictures(q, 1)
