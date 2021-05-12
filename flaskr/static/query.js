fd9cd0696837bf69098680b70f4a2835bc03be38
Niels Bos <nielsbos97@gmail.com>
2021-05-13 01:01:00 +0200

    add query js

---
+++ flaskr/static/query.js
@@ -0,0 +1,15 @@
+hoverarea.addEventListener('mouseover', function() {
+   document.getElementById("hoverarea").getElementsByTagName("img")[0].style.filter = 'grayscale(0%)';
+});
+
+hoverarea.addEventListener('mouseout', function() {
+    document.getElementById("hoverarea").getElementsByTagName("img")[0].style.filter = 'grayscale(100%)';
+ });
+ 
+let div = document.getElementById("hoverarea");
+let height = div.offsetHeight;
+let div_iframe = document.getElementById('playlist');
+div_iframe.style.height = height;
+let iframe = div_iframe.getElementsByTagName('iframe')[0];
+iframe.style.height = height;
+
