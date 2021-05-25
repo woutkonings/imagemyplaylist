7f9fe4d9b5d9a6cd5325043afd4d1d72d0527707
woutkonings <woutkonings@gmail.com>
2021-05-25 22:55:39 +0100

    joe

--- imagemyplaylists/flaskr/helper.py
+++ imagemyplaylists/flaskr/helper.py
@@ -2,7 +2,7 @@ from .config import Config
 
 def determine_host(headers):
     dict_headers = dict(headers)
-    if dict_headers['Host'] == 'pixify.nielsbos.nl':
+    if dict_headers['Host'] == 'spotify.pics':
         return Config.CALLBACK_URL_EXTERNAL
     else:
         return Config.CALLBACK_URL_LOCAL
